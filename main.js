19f3e1bda7c90a813178115144ae94320c1c52f0
liynaamri <b022210261@student.utem.edu.my>
2025-01-23 16:56:46 +0800

    .

--- main.js
+++ main.js
@@ -102,6 +102,6 @@ function verifyToken(req, res, next) {
   }
 }
 
-app.listen(3001, () => {
-  console.log('Server is running on port 3001');
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
